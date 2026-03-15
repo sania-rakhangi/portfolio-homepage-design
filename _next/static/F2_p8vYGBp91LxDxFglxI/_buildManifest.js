c329b1c0c6466339fcc68617da1ad7f511dc4bba
Sania Rakhangi <147305583+sania-rakhangi@users.noreply.github.com>
2026-03-15 22:58:04 +0530

    Updates

--- _next/static/F2_p8vYGBp91LxDxFglxI/_buildManifest.js
+++ _next/static/F2_p8vYGBp91LxDxFglxI/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-homepage-design//_next/:path+",
+        "destination": "/portfolio-homepage-design/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
